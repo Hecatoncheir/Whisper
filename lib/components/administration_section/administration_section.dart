6db50a174a561d979682fe9080fc298dacdf7270
Rasarts <rasart.pro@gmail.com>
2016-09-19 15:19:58 +0300

    Adding administration section component

--- lib/components/administration_section/administration_section.dart
+++ lib/components/administration_section/administration_section.dart
@@ -5,11 +5,25 @@ library administration_section;
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart';
 
+import 'package:web_socket_channel/html.dart';
+
 @PolymerRegister('administration-section')
 class AdministrationSection extends PolymerElement {
+  @Property(reflectToAttribute: true, observer: 'socketMessageChanged')
+  String message;
+
+  @Property(reflectToAttribute: true)
+  Map details;
+
+  @Property(reflectToAttribute: true)
+  HtmlWebSocketChannel socket;
+
   AdministrationSection.created() : super.created();
 
-  attached() {
-    print('attached');
+  @reflectable
+  socketMessageChanged(String newMessage, [_]) {
+    if (newMessage == null) return;
+    print(newMessage);
+    print(details);
   }
 }
