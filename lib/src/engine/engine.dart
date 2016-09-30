cb2086b687e63bf2f02534520b746d2bc2d3a59c
Rasarts <rasart.pro@gmail.com>
2016-10-01 01:02:52 +0300

    Adding pages service to whisper engine

--- lib/src/engine/engine.dart
+++ lib/src/engine/engine.dart
@@ -3,14 +3,19 @@ library engine;
 import 'http/http.dart';
 import 'socket/socket.dart';
 
+import 'package:whisper/microservices/pages/pages.dart' as PagesService;
+
 class Engine extends Object with EngineMixin {
   SocketEngine socket;
+  PagesService.Pages pages;
 
   Engine() {
     socket = new SocketEngine();
+    pages = new PagesService.Pages();
   }
 
   powerUpSockets({String ip, int port}) {
     socket.powerUp();
+    pages.socket.powerUp();
   }
 }
